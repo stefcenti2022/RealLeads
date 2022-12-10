570e9656e608cbea966b067f46b8379df51e2f70
rocket3855 <69988487+rocket3855@users.noreply.github.com>
2022-12-10 13:17:40 -0500

    new files to test routes for census data map

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for
+#from flask_pymongo import PyMongo
+import census_map
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+#mongo = PyMongo(app)
+
+# Set Up App Routes
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+@app.route("/get_map")
+def census_map():
+   # mars = mongo.db.mars
+    #mars_data = scraping.scrape_all()
+    #mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run()
