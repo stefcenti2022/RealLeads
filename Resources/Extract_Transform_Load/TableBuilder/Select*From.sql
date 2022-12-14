4a18d3b4ff03d1e18129ed869cec21948c0be4d8
vjtrom <109374337+vjtrom@users.noreply.github.com>
2022-12-13 22:21:27 -0500

    Vince (#20)
    * ID_Table
    
    * Lat_Lng_Data
    
    * Delete .DS_Store
    
    * Adding ETL files and data
    
    Co-authored-by: Vincent Trombetta <vincenttrombetta@Vincents-MBP-2.hsd1.de.comcast.net>
    Co-authored-by: stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>

---
+++ Resources/Extract_Transform_Load/TableBuilder/Select*From.sql
@@ -0,0 +1,7 @@
+SELECT * FROM broker_data_clean;
+SELECT * FROM id_table;
+SELECT * FROM lat_lng_clean;
+SELECT * FROM mortgage_data_clean;
+SELECT * FROM prop_charac_clean;
+SELECT * FROM pub_rec_clean;
+SELECT * FROM sales_data_clean;
